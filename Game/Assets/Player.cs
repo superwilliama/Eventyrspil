050e78dd60ee02ae240756308b88519826e9cbeb
thom989t <thomas.halgaard.ditlevsen@gmail.com>
2023-11-10 17:27:45 +0100

    Update Player.cs
    player movement is done (I think)

--- Game/Assets/Player.cs
+++ Game/Assets/Player.cs
@@ -4,14 +4,46 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
 
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float speed = 5;
 
     Vector2 movement;
-
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
+
+
         movement.y = Input.GetAxisRaw("Vertical");
+
+        bool W = Input.GetKey(KeyCode.W);
+        bool A = Input.GetKey(KeyCode.A);
+        bool S = Input.GetKey(KeyCode.S);
+        bool D = Input.GetKey(KeyCode.D);
+        Vector2 pos = new Vector2(0,0);
+        
+        if (W)
+        {
+            pos.y += 1;
+        }
+
+        if (A)
+        { 
+            pos.x += -1; 
+        }
+
+        if (S)
+        { 
+            pos.y += -1; 
+        }
+
+        if (D)
+        { 
+            pos.x += 1;
+        }
+        pos = pos.normalized;
     }
 
     private void FixedUpdate()
